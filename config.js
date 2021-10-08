77ff361d4361e7ae15367db9592f76bf1f828a62
lailaphananh <91012393+lailaphananh@users.noreply.github.com>
2021-10-09 00:30:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,19 @@
 const CONFIG = {
-    introTitle: 'Hii ChiChi!',
+    introTitle: 'Babe à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao Chi cứ đi nhầm đường
-    Lạc vào tim tớ lẻ loi`,
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Trêu Chi tí thôi à 😙',
-    desc: 'Chi có muốn trò chuyện với pa chút ko ',
-    btnYes: 'Cóoo🥰🥰🥰',
-    btnNo: 'Khum😣😣😣',
-    question:'UwU',
-    btnReply: 'Gửi cho pa',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    messDesc: 'Mình bắt đầu nhé.',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/messages/t/100026031100253' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
